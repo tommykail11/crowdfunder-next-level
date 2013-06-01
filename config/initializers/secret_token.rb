5847c4c447239ca15320ac3e3aec0fd5407f0f8e
Tommy Kail <tommykail@gmail.com>
2013-06-01 11:46:47 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrowdfunderNextLevel::Application.config.secret_token = 'cfe24cf2695961ee4732007653b2f766da0cdb3b0f1a451d6a56b11ffac812d607e5f837276739f6dcb56f809910cfa4096be3cb74bd6c9645e07f841c6fd8c6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CrowdfunderNextLevel::Application.config.session_store :cookie_store, key: '_crowdfunder-next-level_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CrowdfunderNextLevel::Application.config.session_store :active_record_store
