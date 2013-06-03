d5092f7db4b627933bfef42c57a18cead1d4508a
Tommy Kail <tommykail@gmail.com>
2013-06-02 21:20:41 -0400

    Seeding database with dummy info

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.destroy_all
+Project.destroy_all
+
+user = User.create!(first_name: "John", last_name: "Doe", email: "john@doe.com", password: "password")
+
+project1 = user.projects.create!(title: "Project 1", teaser: "Teaser text 1",
+  description: "Description 1", goal: 13000)
+
+project2 = user.projects.create!(title: "Project 2", teaser: "Teaser text 2",
+  description: "Description 2", goal: 210000)
+
+project3 = user.projects.create!(title: "Project 3", teaser: "Teaser text 3",
+  description: "Description 3", goal: 30000)
+
