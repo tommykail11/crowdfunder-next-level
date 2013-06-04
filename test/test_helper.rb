75e77788e92fd6887cb06ff875dce951cf9db236
Tommy Kail <tommykail@gmail.com>
2013-06-04 15:25:38 -0400

    Writing tests for user registration

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,4 +6,9 @@ class ActionDispatch::IntegrationTest
   # Make the Capybara DSL available in all integration tests
   include Capybara::DSL
   Capybara.app = CrowdfunderNextLevel::Application
+
+  teardown do
+    Capybara.reset_sessions!    # Forget the (simulated) browser state
+    Capybara.use_default_driver # Revert Capybara.current_driver to Capybara.default_driver
+  end
 end
