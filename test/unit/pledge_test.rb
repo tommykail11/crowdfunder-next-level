34c0455ae4b82c664b314b6ff8be8b7f4786fdf1
Tommy Kail <tommykail@gmail.com>
2013-06-05 17:49:41 -0400

    Writing tests for Pledge model

--- test/unit/pledge_test.rb
+++ test/unit/pledge_test.rb
@@ -1,7 +1,44 @@
 require 'test_helper'
 
 class PledgeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  # Validations
+  test "requires a user" do
+    pledge = FactoryGirl.build(:pledge, user: nil)
+
+  # Should not be able to save
+  assert_false pledge.save
+  asser_equal "can't be blank", pledge.errors[:user].first
+  end
+
+  test "requires a project" do
+    pledge = FactoryGirl.build(:pledge, project: nil)
+
+    # Should not be able to save
+    assert_false pledge.save
+    assert_equal "can't be blank", pledge.errors[:user].first
+  end
+
+  test "requires amount" do
+    pledge = FactoryGirl.build(:pledge, amount: nil)
+
+    assert_false pledge.save
+    assert_equal "is not a number", pledge.errors[:amount].first
+  end
+
+  test "requires amount greater than 0" do
+    pledge = FactoryGirl.build(:pledge, amount: -10)
+
+    # Should not be able to save
+    assert_false pledge.save
+    assert_equal "must be greater than 0", pledge.errors[:amount].first
+  end
+
+  test "requires amount to be an integer" do
+    pledge = FactoryGirl.build(:pledge, amount: 'abc')
+
+    # Should not be able to save
+    assert_false pledge.save
+    assert_equal "is not a number", pledge.errors[:amount].first
+  end
 end
