1c157cb099a6bd32db5b31f6570f2ac3f170f7bc
Tommy Kail <tommykail@gmail.com>
2013-06-10 12:39:07 -0400

    Writing tests to delete projects

--- test/integration/my/project_flows_test.rb
+++ test/integration/my/project_flows_test.rb
@@ -75,4 +75,22 @@ class My::ProjectFlowsTest < ActionDispatch::IntegrationTest
     assert_equal new_my_project_path, current_path
     assert_equal "My Projects", find('.navbar ul li.active a').text
   end
+
+  test "can delete my project" do
+    Capybara.current_driver = Capybara.javascript_driver
+
+    me = setup_signed_in_user
+    project = FactoryGirl.create :project, user: me
+
+    visit edit_my_project_path(project)
+
+    assert has_link?('Delete Project')
+    click_link 'Delete Project'
+
+    page.driver.accept_js_confirms! # Warning: this is specific to webkit driver
+
+    assert_page.has_content? ('deleted')
+
+    assert_nil Project.find_by_id(project.id)
+  end
 end
